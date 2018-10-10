ecd0e6d93b501f06d6d8dd5f59c9bb0a4f86b678
GuillaumeMagnette <guillaume.magnette8@etu.univ-lorraine.fr>
2018-10-10 12:00:16 +0200

    la classe Mur

---
+++ jav/src/Mur.java
@@ -0,0 +1,15 @@
+public class Mur {
+	
+	private int posx, posy, longx, longy;
+
+	public Mur(int posx, int posy, int longx, int longy){
+		this.posx = posx;
+		this.posy = posy;
+		this.longx = longx;
+		this.longy = longy;
+	}
+	
+	
+	
+
+}
--- jav/src/Principal.java
+++ jav/src/Principal.java
@@ -6,7 +6,7 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 }
