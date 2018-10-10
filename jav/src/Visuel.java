b2698f18d8f75559250f8756b987098c2f3ed283
AlexandrePerdereau <alexandre.perdereau9@etu.univ-lorraine.fr>
2018-10-10 20:22:02 +0200

    deuxieme version de Visuel.java

--- jav/src/Visuel.java
+++ jav/src/Visuel.java
@@ -4,6 +4,9 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 
 public class Visuel extends JFrame {
 
@@ -29,12 +32,25 @@ public class Visuel extends JFrame {
 	 * Create the frame.
 	 */
 	public Visuel() {
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 500);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(59, 79, 395, 345);
+		contentPane.add(panel);
+		
+		JButton btnCommencerLeNiveau = new JButton("Commencer le niveau");
+		btnCommencerLeNiveau.setBounds(298, 13, 157, 54);
+		contentPane.add(btnCommencerLeNiveau);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(126, 29, 127, 22);
+		contentPane.add(comboBox);
 	}
-
 }
