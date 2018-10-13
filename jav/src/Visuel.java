68cec1793f55ac81c62f0d40728b36065c69b46c
AlexandrePerdereau <alexandre.perdereau9@etu.univ-lorraine.fr>
2018-10-13 17:47:55 +0200

    fenetre

--- jav/src/Visuel.java
+++ jav/src/Visuel.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,11 +12,15 @@ import javax.swing.JComboBox;
 public class Visuel extends JFrame {
 
 	private JPanel contentPane;
+	deplacement.Heros perso = new deplacement.Heros(10,10,10);
+	ArrayList<Mur> batiment = new ArrayList<Mur>();
+	boolean partieencours = false ;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -34,7 +39,7 @@ public class Visuel extends JFrame {
 	public Visuel() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 500, 500);
+		setBounds(100, 100, 600, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -42,7 +47,7 @@ public class Visuel extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(59, 79, 395, 345);
+		panel.setBounds(100, 100, this.getWidth()-200, this.getHeight()-200);
 		contentPane.add(panel);
 		
 		JButton btnCommencerLeNiveau = new JButton("Commencer le niveau");
@@ -52,5 +57,11 @@ public class Visuel extends JFrame {
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(126, 29, 127, 22);
 		contentPane.add(comboBox);
+	}
+	
+	public void anim(){
+		while (partieencours){
+			
+		}
 	}
 }
