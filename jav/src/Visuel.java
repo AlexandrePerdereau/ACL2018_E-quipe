25c65b8bfa78f04414812221509922d7fafa2729
AlexandrePerdereau <alexandre.perdereau9@etu.univ-lorraine.fr>
2018-10-16 21:24:26 +0200

    combobox version 2

--- jav/src/Visuel.java
+++ jav/src/Visuel.java
@@ -3,6 +3,10 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Panel;
 import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -19,6 +23,8 @@ public class Visuel extends JFrame {
 	deplacement.Heros perso = new deplacement.Heros(10,10,10);
 	ArrayList<Mur> batiment = new ArrayList<Mur>();
 	boolean partieencours = false ;
+	
+	
 
 	/**
 	 * Launch the application.
@@ -41,6 +47,8 @@ public class Visuel extends JFrame {
 	 * Create the frame.
 	 */
 	public Visuel() {
+		
+		
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 600);
@@ -64,10 +72,20 @@ public class Visuel extends JFrame {
 			}
 		});
 		
+		
 		JComboBox comboBox = new JComboBox();
 		comboBox.setBounds(126, 29, 127, 22);
 		contentPane.add(comboBox);
+		comboBox.addItem("niveau 1");
+		comboBox.addItemListener(new combo());
+		
+		
 	}
+	class combo implements ItemListener{
+	    public void itemStateChanged(ItemEvent e) {
+	      System.out.println("événement déclenché sur : " + e.getItem());
+	    }               
+	  }
 	
 	public void anim(){
 		while (partieencours){
@@ -81,4 +99,5 @@ public class Visuel extends JFrame {
 			}
 		}
 	}
+	
 }
