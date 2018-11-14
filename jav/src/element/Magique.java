2a8592e8393bfbb215f871c9d559e00654dca6e6
Ludovic Gelli <gelli1u@fst-ats-303-l02.fst.site.univ-lorraine.fr>
2018-11-14 11:35:35 +0100

    .

--- jav/src/element/Magique.java
+++ jav/src/element/Magique.java
@@ -10,38 +10,33 @@ public class Magique extends Element_marchable{
 		this.effect = effect;
 	}
 
-	public Magique(int x, int y, int LongX,int LongY,String s) {
+	public Magique(int x, int y, int LongX,int LongY, String s) {
 		super(x, y, LongX, LongY);
 		this.effect=s;
 	}
 
 	public void appeffect(Heros h){
 		System.out.println("effetapp:"+effect);
+		if (!effect.contains("E")){
+			String[] s = this.effect.split("E"); // Sépare l'intitulé de l'effet de la valeur de son intensité : "{type d'effet} E {intensité de l'effet}"
+			for (String so:s)System.out.println(so);
 
-		String[] s = this.effect.split("E");
-		for (String so:s)System.out.println(so);
+			if (s[0].equals("vitesse")){
 
-		if (s[0].equals("vitesse")){
-
-			h.setFacteurdevitesse(h.getFacteurdevitesse()*Integer.parseInt(s[1]));
-			System.out.println("vitesse*"+s[1]);
-		}
+				h.setFacteurdevitesse(h.getFacteurdevitesse()*Integer.parseInt(s[1]));
+				System.out.println("vitesse*"+s[1]);
+			}
 		
-		else if (s[0].equals("soin")){
-			
-		}
+			else if (s[0].equals("soin")){
+				h.setPointdevie(h.getPointdevie() + Integer.parseInt(s[2]));
+			}
 		
-		else if (s[0].equals("tuermonstre")){
-			
+			else if (s[0].equals("piege")){
+				h.setPointdevie(h.getPointdevie() - Integer.parseInt(s[2]));
+			}
 		}
-		
-
-
 
 	}
 
-	
-
-	
 
 }
